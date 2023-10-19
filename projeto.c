6e62961dc13f1b6c5d6429659d52086956659203
Jessica <fedaltojessica@gmail.com>
2023-10-19 09:42:57 -0300

    Case 2 and 3

--- projeto.c
+++ projeto.c
@@ -13,6 +13,11 @@ typedef struct
 
 }desafio;
 
+typedef struct 
+{
+    char p1[50];
+    float valor;
+}carrinho;
 
 
 
@@ -22,12 +27,22 @@ int main()
     int op;
     int tamanho;
     int x = 0;
+    int quantidade_produtos = 0;
+    int y = 0;
+    int w = 0;
+    char nome[50];
+    int qtd = 0;
+    float soma[5];
+    float total;
+
+    FILE * arquivo;
 
     printf("\nQuantos materias diferentes a empresa fornece? ");
     scanf("\n%d", &tamanho);
 
+    arquivo = fopen("notafiscal.txt", "a");
 
-    desafio * cadastrar = malloc(tamanho*sizeof(char));
+    desafio * cadastrar = malloc(tamanho*sizeof(desafio));
 
     while(op != 0)
     {
@@ -36,6 +51,7 @@ int main()
         printf("\n-----2. carrinho--------");
         printf("\n-----3. orçamento-------");
         printf("\n-----4. adicionar novo produto-----");
+        printf("\n-------5. adicionar ao carrinho ------");
         scanf("\n%d", &op);
 
         switch (op)
@@ -44,9 +60,9 @@ int main()
             for (int i = x; i < tamanho; i++)
             {
                 pritf("\nNome do material: ");
-                scanf("\n%s", cadastrar[i].produto);
+                scanf("%[^\n]", cadastrar[i].produto);
                 printf("\nPreço: ");
-                scanf("\n%f", cadastrar[i].preco);
+                scanf("%[^\n]", cadastrar[i].preco);
 
                 x = x + 1;
             }
@@ -55,12 +71,52 @@ int main()
         case 2:
             for (int i = 0; i < tamanho; i++)
             {
-                printf("\nProduto: %s", cadastrar[i].produto);
+                printf("\n %d - Produto: %s", i, cadastrar[i].produto);
                 printf("\tPreco: %2.f", cadastrar[i].preco);
             }
-            
+            printf("\nQuantos produtos diferentes serao adicionados ao carrinho: ");
+            scanf("%d", &quantidade_produtos);
 
+            carrinho * escolhas = malloc(quantidade_produtos*sizeof(carrinho));
+
+            for (int i = y; i < quantidade_produtos; i++)
+            {
+                printf("\n Qual o nome produto desejado? ");
+                scanf("%[^\n]", nome);
+                for (int i = 0; i < tamanho; i++)
+                {
+                    if (nome == cadastrar[i].produto)
+                    {
+                        //escolhas[w]->p1 = cadastrar[i].produto;
+                        escolhas[w].valor = cadastrar[i].preco;
+                        w = w + 1;
+                    }
+                }
+                y = y + 1;
+            }
+            
             break;
+
+        case 3:
+            for (int i = 0; i < quantidade_produtos; i++)
+            {
+                printf("\nQual a quantidade do %d produto? ", i+1);
+                scanf("%d", &qtd);
+                soma[i] = qtd * escolhas[i].valor;
+            }
+            for (int i = 0; i < quantidade_produtos; i++)
+            {
+                total = soma[i]++;
+            }
+            
+            printf("\nO total e: %2.f", total);
+            
+            break;
+        case 4:
+            printf("Quantos itens serao no total? ");
+            scanf("%d", &tamanho);
+            cadastrar = realloc(cadastrar, tamanho*sizeof(desafio));
+            
         }
     }
 }
